3d7473579e7006c35098947511459f86b75b320f
Innocent <itnhamo@gmail.com>
2023-12-20 12:17:12 -0800

    first commit

---
+++ src/AppData.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        id : 1, 
+        question : "What is Frontend Mentor, and how will it help me?",
+        answer : "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. it's suitable for all levels and ideal for portfolio building."
+    },
+    {
+        id : 2,
+        question : "Is Frontend Mentor free?", 
+        answer : "Frontend Mentor is partially free. Most of the projects are free and courses. However there are paid for premiums."
+    },
+    {
+        id : 3, 
+        question : "Can I use Frontend Mentor projects in my portfolio?",
+        answer : ""
+    },
+    {
+        id : 4,
+        question : "How can I get help if I'm stuck on a challenge?",
+        answer : ""
+    }
+]
